1b7154768927214ea64a08eb3754e30573a2d6f4
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-21 21:40:26 -0500

    validando fecha de cliente

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,3 +1,25 @@
 
 
+//* Entrada de fecha de nacimiento
+
+const inputNacimiento = document.querySelector("#birth"); //seleccionar el elemento con el id = birth
+
+inputNacimiento.addEventListener("blur", (evento) => {
+validarNacimiento(evento.target);
+});
+
+//* Valor fecha de nacimiento
+
+function validarNacimiento(input) {
+const fechaCliente = new Date (input.value); //tomar el valor del input de fecha
+mayorDeEdad(fechaCliente);
+}
+
+//* Verificar si la fecha ingresada cumple con la condicion de edad >18 years
+
+function mayorDeEdad(fecha) {
+  const fechaActual = new Date();
+ console.log(fecha,"---",fechaActual);
+}
+
 
